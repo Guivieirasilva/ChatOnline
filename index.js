a08959901007066a03c8f66f6a99000b71ea3b62
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-07-04 19:55:34 -0300

    mandando mensagens

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const PORT = 3000
+const PORT = 3000;
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const socketIo = require('socket.io');
+const messages = [];
 
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT, () => {
-    console.log(`Server Running localhost:${PORT}`);}
-)   
+const server = app.listen(PORT , () => {
+    console.log(`Server Running localhost:${PORT}`);
+
+});
+
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+    console.log('New Connection');
+    socket.emit('Update_messages', messages)
+
+
+    socket.on('new_message', (data) => {
+        messages.push(data.msg)
+    })
+
+    io.emit('Update_messages', messages)
+});
